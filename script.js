40c8a46bde233021e52f2d5d0a0e5317c0a2b5be
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-04-18 19:16:23 +0300

    Display books on screen

--- script.js
+++ script.js
@@ -15,11 +15,45 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function displayBooksOnScreen() {
+function createCard(cardNumber) {
   const cards = document.querySelector(".cards");
-  for (let element of myLibrary) {
-    const div = document.createElement("div");
-    div.classList.add("card");
-    cards.appendChild(div);
+  const div = document.createElement("div");
+  div.classList.add(`card-${cardNumber}`);
+  cards.appendChild(div);
+}
+
+function addCardElements(cardNumber) {
+  const card = document.querySelector(`.card-${cardNumber}`);
+  const pTitle = document.createElement("p");
+  const pAuthor = document.createElement("p");
+  const pPages = document.createElement("p");
+  const buttonIsRead = document.createElement("button");
+  const buttonDelete = document.createElement("button");
+
+  pTitle.classList.add(`title-${cardNumber}`);
+  pAuthor.classList.add(`author-${cardNumber}`);
+  pPages.classList.add(`pages-${cardNumber}`);
+  buttonIsRead.classList.add(`buttonIsRead-${cardNumber}`);
+  buttonDelete.classList.add(`buttonDelete-${cardNumber}`);
+
+  pTitle.textContent = myLibrary[cardNumber].title;
+  pAuthor.textContent = myLibrary[cardNumber].author;
+  pPages.textContent = myLibrary[cardNumber].pages;
+  buttonIsRead.textContent = "Read";
+  buttonDelete.textContent = "Remove";
+
+  card.appendChild(pTitle);
+  card.appendChild(pAuthor);
+  card.appendChild(pPages);
+  card.appendChild(buttonIsRead);
+  card.appendChild(buttonDelete);
+}
+
+function displayBooksOnScreen() {
+  for (let i in myLibrary) {
+    createCard(i);
+    addCardElements(i);
   }
 }
+
+displayBooksOnScreen();
